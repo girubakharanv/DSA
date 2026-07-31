f77919fbe450842ddbf5dba5cb551939b364e7b3
girubakharanv <girubakharanv@gmail.com>
2026-07-31 20:02:47 +0530

    Array

--- single_number.java
+++ single_number.java
@@ -16,3 +16,13 @@ public class single_number {
         System.out.println(ans);
     }
 }
+
+/*
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+*/
