ea5fe366a3ba1447834f10f580d9668c95f99cab
girubakharanv <girubakharanv@gmail.com>
2026-08-12 12:27:20 +0530

    Array

---
+++ leetcode1389.java
@@ -0,0 +1,61 @@
+/*
+->Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+=>Initially target array is empty.
+->From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+->Repeat the previous step until there are no elements to read in nums and index.
+->Return the target array.
+
+->It is guaranteed that the insertion operations will be valid.
+*/
+import java.util.ArrayList;
+class leetcode1389 {
+   static public int[] createTargetArray(int[] nums, int[] index) {
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            list.add(index[i],nums[i]);
+        }
+        int[] arr=new int[list.size()];
+        int ind=0;
+        for(int i:list){
+            arr[ind]=i;
+            ind++;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        leetcode1389 obj = new leetcode1389();
+        int[] nums = {0,1,2,3,4};
+        int[] index = {0,1,2,2,1};
+        int[] ans = obj.createTargetArray(nums,index);
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+}
+ 
+ /*
+ ->Example 1:
+
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+Explanation:
+nums       index     target
+0            0        [0]
+1            1        [0,1]
+2            2        [0,1,2]
+3            2        [0,1,3,2]
+4            1        [0,4,1,3,2]
+
+->Example 2:
+
+Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
+Output: [0,1,2,3,4]
+Explanation:
+nums       index     target
+1            0        [1]
+2            1        [1,2]
+3            2        [1,2,3]
+4            3        [1,2,3,4]
+0            0        [0,1,2,3,4]
+*/
