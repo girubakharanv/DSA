b32622287e2dbd0d7320946381a78748bb1c162d
girubakharanv <girubakharanv@gmail.com>
2026-08-12 11:51:21 +0530

    Array

---
+++ leetcode_1295.java
@@ -0,0 +1,49 @@
+/*
+->Given an array nums of integers, return how many of them contain an even number of digits.
+*/
+class leetcode_1295 {
+    public int findNumbers(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            int digcount=0;
+            while(nums[i]!=0){
+                nums[i]=nums[i]/10;
+                digcount++;
+            }
+            if(digcount%2==0){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        leetcode_1295 obj = new leetcode_1295();
+        int[] nums = {12,345,2,6,7896};
+        int ans = obj.findNumbers(nums);
+        System.out.println(ans);
+    }
+}
+
+/*
+->Example 1:
+
+Input: nums = [12,345,2,6,7896]
+Output: 2
+Explanation: 
+12 contains 2 digits (even number of digits). 
+345 contains 3 digits (odd number of digits). 
+2 contains 1 digit (odd number of digits). 
+6 contains 1 digit (odd number of digits). 
+7896 contains 4 digits (even number of digits). 
+Therefore only 12 and 7896 contain an even number of digits.
+
+->Example 2:
+
+Input: nums = [555,901,482,1771]
+Output: 1 
+Explanation: 
+Only 1771 contains an even number of digits.
+*/ 
